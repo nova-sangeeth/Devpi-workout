2100e3fc983cb38f821d317f8e2401487e7da083
nova-sangeeth <novasangeeth@outlook.com>
2023-02-23 00:00:57 +0530

    Hold the changes for now.

--- containerized/create_index.sh
+++ containerized/create_index.sh
@@ -1,9 +1,9 @@
 
-devpi use http://localhost:3141
-devpi login root --password=
-devpi user --create pypi email= password=${PYPI_PASSWORD:-}
-devpi user --list
-devpi index --create pypi/stable bases=root/pypi volatile=True mirror_whitelist=*
-devpi index --list
+# devpi use http://localhost:3141
+# devpi login root --password=
+# devpi user --create pypi email= password=${PYPI_PASSWORD:-}
+# devpi user --list
+# devpi index --create pypi/stable bases=root/pypi volatile=True mirror_whitelist=*
+# devpi index --list
 
 echo "skipping the index creation for now "
